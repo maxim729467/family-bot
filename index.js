31bec7c03bceff2860ace1eaef98f93d8c8ce641
Maksym Shyshko <maxim.shishko95@gmail.com>
2024-04-22 22:40:48 +0300

    update mf

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-const express = require("express");
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
-const {
-  scheduleForecastSend,
-  scheduleFarewell,
-} = require("./methods/schedule");
-const { sendQuestion } = require("./methods/api");
-const { cutQuestion } = require("./methods/helpers");
+const { scheduleForecastSend, scheduleFarewell } = require("./methods/schedule");
+// const { sendQuestion } = require("./methods/api");
+// const { cutQuestion } = require("./methods/helpers");
 const { TELEGRAM_TOKEN, BOT_ID } = process.env;
 
 const bot = new Telegraf(TELEGRAM_TOKEN);
-
-const app = express();
-const PORT = process.env.PORT || 8080;
 
 // bot.use((ctx, next) => {
 // console.log(ctx.update.message.text);
 // next();
 // })
 
-bot.start((ctx) => {
-  if (global.isInitiated) {
-    ctx.reply("Tasks have been already scheduled.");
-    return;
-  }
-
-  ctx.reply("Scheduling tasks...");
-  scheduleForecastSend(bot);
-  scheduleFarewell(bot);
-  global.isInitiated = true;
-});
+scheduleForecastSend(bot);
+scheduleFarewell(bot);
 
 // bot.help(ctx => ctx.reply('Help command'));
 // bot.settings(ctx => ctx.reply('Settings command'));
@@ -38,31 +22,22 @@ bot.start((ctx) => {
 //     ctx.telegram.copyMessage(ctx.chat.id, ctx.message.from.id, ctx.message.message_id);
 // })
 
-bot.on("message", async (ctx) => {
-  const { message } = ctx.update;
-  const msgContent = message.text ? message.text.toLowerCase() : "";
+// bot.on("message", async (ctx) => {
+//   const { message } = ctx.update;
+//   const msgContent = message.text ? message.text.toLowerCase() : "";
 
-  const isReplyToBot =
-    message.reply_to_message && message.reply_to_message.from.is_bot;
-  const isBotMentioned =
-    cutQuestion(msgContent).length &&
-    msgContent.startsWith(BOT_ID) &&
-    !msgContent.includes("/start");
+//   const isReplyToBot =
+//     message.reply_to_message && message.reply_to_message.from.is_bot;
+//   const isBotMentioned =
+//     cutQuestion(msgContent).length &&
+//     msgContent.startsWith(BOT_ID) &&
+//     !msgContent.includes("/start");
 
-  if (isReplyToBot || isBotMentioned) {
-    const question = isBotMentioned ? cutQuestion(msgContent) : msgContent;
-    await sendQuestion(question, ctx);
-  }
-});
+//   if (isReplyToBot || isBotMentioned) {
+//     const question = isBotMentioned ? cutQuestion(msgContent) : msgContent;
+//     await sendQuestion(question, ctx);
+//   }
+// });
 
 // bot.start(ctx => ctx.replyWithMarkdownV2('Hey, this is inline bot'))
 // bot.on('inline_query', async ctx => {})
-
-app.use("/", (req, res) => {
-  res.send("Mafia bot is launched");
-});
-
-app.listen(PORT, () => {
-  console.log("Launching mafia bot...");
-  bot.launch();
-});
--- methods/schedule.js
+++ methods/schedule.js
@@ -1,34 +1,28 @@
-const cron = require('node-cron');
-const { 
-     sendForecast,
-     generateForecastErrorMsg,
-     generateFarewell,
-     generateGreeting,
-     } = require('../methods/helpers');
+const cron = require("node-cron");
+const { sendForecast, generateForecastErrorMsg, generateFarewell, generateGreeting } = require("../methods/helpers");
 
 exports.scheduleForecastSend = (bot) => {
-    console.log('scheduling forecast');
-    cron.schedule('0 7 * * *', async () => {
-    console.log('running scheduled task...');
+  console.log("scheduling forecast");
+  cron.schedule("0 8 * * *", async () => {
+    console.log("running scheduled task...");
 
     try {
-        const chatId = process.env.CHAT_ID;
-        bot.telegram.sendMessage(process.env.CHAT_ID, generateGreeting());
-        await sendForecast('Odessa', bot, chatId);
-        await sendForecast('Chisinau', bot, chatId);
-        await sendForecast('Mersin', bot, chatId);
-        
+      const chatId = process.env.CHAT_ID;
+      bot.telegram.sendMessage(process.env.CHAT_ID, generateGreeting());
+      await sendForecast("Odessa", bot, chatId);
+      await sendForecast("Chisinau", bot, chatId);
+      await sendForecast("Mersin", bot, chatId);
     } catch (error) {
-        console.log(error);
-        bot.telegram.sendMessage(process.env.CHAT_ID, generateForecastErrorMsg());
+      console.log(error);
+      bot.telegram.sendMessage(process.env.CHAT_ID, generateForecastErrorMsg());
     }
-      }); 
-}
+  });
+};
 
 exports.scheduleFarewell = (bot) => {
-    console.log('scheduling farewell');
-    cron.schedule('0 22 * * *', () => {
-    console.log('running scheduled task...');
+  console.log("scheduling farewell");
+  cron.schedule("0 22 * * *", () => {
+    console.log("running scheduled task...");
     bot.telegram.sendMessage(process.env.CHAT_ID, generateFarewell());
-      }); 
-}
+  });
+};
